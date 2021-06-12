c4c6b81bbe4827566fcc11d5195c8b6085697262
StariyWah <83608617+StariyWah@users.noreply.github.com>
2021-06-12 23:03:12 +0300

    Add files via upload

---
+++ work.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+require_once('sprites.html');
+require_once('header.html');
+require_once('work-5.html');
+require_once('footer.html');
+?>   
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
